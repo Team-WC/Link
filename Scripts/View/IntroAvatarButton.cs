3a944957b3b9d5c024d27038516b6316978d969d
ChanHyeong Jung <chanh327@gmail.com>
2018-04-19 23:42:45 +0900

    v0.5.3 Add select avatar & enter name

---
+++ Scripts/View/IntroAvatarButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class IntroAvatarButton : UIBehaviour
+{
+    private IntroController introController;
+
+    private Button[] avatarButton;
+    private ColorBlock nColor;
+    private ColorBlock pColor;
+
+    protected override void Awake()
+    {
+        introController = GameObject.Find("Intro").GetComponent<IntroController>();
+        avatarButton = GetComponentsInChildren<Button>();
+
+        nColor = avatarButton[0].colors;
+        pColor = nColor;
+        pColor.normalColor = pColor.pressedColor;
+        pColor.highlightedColor = pColor.pressedColor;
+        avatarButton[0].colors = pColor;
+    }
+
+    public void OnPressedButton(int num)
+    {
+        for (int i = 0; i < avatarButton.Length; i++)
+        {
+            if (i == num)
+            {
+                avatarButton[i].colors = pColor;
+                introController.playerAvatarNumber = num;
+            }
+            else
+            {
+                avatarButton[i].colors = nColor;
+            }
+        }
+    }
+}
--- Scripts/View/IntroController.cs
+++ Scripts/View/IntroController.cs
@@ -7,47 +7,43 @@ public class IntroController : UIBehaviour
     public GameObject[] introPage;
     public int currentPage = 0;
 
+    public int playerAvatarNumber = 0;
+    public string playerName = "Player";
+    public InputField inputField;
+
     protected override void Start()
     {
         ChangePage(currentPage);
+
+        inputField = GameObject.Find("InputField").GetComponent<InputField>();
     }
 
     public void NextPage()
     {
-		currentPage++;
-		ChangePage(currentPage);
+        currentPage++;
+        ChangePage(currentPage);
     }
 
     public void PrevPage()
     {
-		currentPage--;
-		ChangePage(currentPage);
+        currentPage--;
+        ChangePage(currentPage);
     }
 
-	public void EndIntro()
-	{
+    public void EndIntro()
+    {
         ScenarioManager.instance.playerProgress.intro = true;
 
+        playerName = inputField.text;
+
+        // user avatar, user name  
+        Debug.Log(playerAvatarNumber + " " + playerName);
+
         this.gameObject.SetActive(false);
-	}
+    }
 
     public void ChangePage(int page)
     {
-		if(page < 0 || introPage.Length <= page)
-		{
-			Debug.LogError("Out of range intro page");
-		}
-
-        for (int i = 0; i < introPage.Length; i++)
-        {
-            if (i == page)
-            {
-                introPage[i].SetActive(true);
-            }
-            else
-            {
-				introPage[i].SetActive(false);
-            }
-        }
+        introPage[page].transform.SetAsLastSibling();
     }
 }
